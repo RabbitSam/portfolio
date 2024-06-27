55a6eb04b04e44945c6e5d6ceec7cbec9f65c3cf
RabbitSam <aquibmahmood1637@gmail.com>
2024-06-27 14:42:37 -0400

    Converted js animations to css in HorizontalAnimatedText for better performance

--- portfolio_next/components/HorizontalAnimatedText.js
+++ portfolio_next/components/HorizontalAnimatedText.js
@@ -1,21 +1,15 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
-export default function HorizontalAnimatedText({ children, direction, duration, delay }) {
+export default function HorizontalAnimatedTextV2({ children, direction }) {
+    const [visible, setVisible] = useState(false);
+    const targetRef = useRef(null);
+    const textRef = useRef(null);
+
     const translateClasses = {
         "left": "-translate-x-[110vw]",
         "right": "translate-x-[110vw]",
     };
-
-    const translateStyles = {
-        "left": "-110vw",
-        "right": "110vw"
-    }
-
-    const animationFill = "forwards";
-
-    const targetRef = useRef(null);
-    const textRef = useRef(null);
 
     useEffect(() => {
         const options = {
@@ -32,51 +26,32 @@ export default function HorizontalAnimatedText({ children, direction, duration, 
             if (isFullyIntersecting) {
                 // Animate to reveal
                 if (textRef.current && targetRef.current && targetRef.current.dataset.revealed !== "true") {
-                    const textKeyframes = [
-                        {
-                            transform: "translateX(0%)",
-                            filter: "none",
-                            easing: "ease-in-out"
-                        }
-                    ];
-
-                    textRef.current.animate(textKeyframes, {fill: animationFill, duration, delay});
-                    targetRef.current.dataset.revealed = "true";
+                    setVisible(true);
                 }
 
             } else if (isContentBelow) {
                 // Animate to hide
                 if (textRef.current && targetRef.current) {
-                    const textKeyframes = [
-                        {
-                            transform: `translateX(${translateStyles[direction]})`,
-                            filter: "blur(4px)",
-                            easing: "ease-in-out"
-                        }
-                    ];
-
-                    textRef.current.animate(textKeyframes, {fill: animationFill, duration});
-                    targetRef.current.dataset.revealed = "false";
+                    setVisible(false);
                 }
             } else if (isContentAbove) {
-                // Set to revealed
-                if (textRef.current && targetRef.current) {
-                    textRef.current.style.transform = `translateX(0%)`;
-                    textRef.current.style.filter = "none";
-
-                    targetRef.current.dataset.revealed = "true";
-                }
+                setVisible(true);
             }
         });
 
         const observer = new IntersectionObserver(reveal, options);
         observer.observe(targetRef.current);
 
+        return () => {
+            observer.unobserve(targetRef.current);
+        };
+
     }, []);
+
 
     return (
         <div className="relative">
-            <div ref={textRef} className={`${translateClasses[direction]} blur-sm`}>{children}</div>
+            <div ref={textRef} className={`${visible ? "" : `${translateClasses[direction]} blur-sm`} transition-all `}>{children}</div>
             {/* This second one helps to trigger the animation */}
             <div ref={targetRef} data-revealed="false" aria-hidden="true" className="opacity-0 select-none -z-50 absolute top-0 left-0">{children}</div>
         </div>
